5fc096f7051f84396b533b94b684526628b40154
unknown <scgodbol@EECS-katya.adsroot.itcs.umich.edu>
2014-11-02 11:47:39 -0500

    Got a basic timer working

--- Assets/Scripts/DecisionBehaviour.cs
+++ Assets/Scripts/DecisionBehaviour.cs
@@ -9,16 +9,31 @@ public class DecisionBehaviour : MonoBehaviour {
 	public GameObject[] WActionSet;
 	public int example = 5;
 
+	// Stuff for the countdown
+	public float pickTime = 3f;
+	public GUIText text;
+	private bool collided;
+	private float timer;
+	private int dir;
 
 	// Use this for initialization
 	void Start () {
+		collided = false;
 		DisableActions ();
 		gameObject.renderer.material.color = new Color(1.0f,1.0f,1.0f,0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(collided) {
+			timer -= Time.deltaTime;
+			text.text = timer.ToString ();
+			if (timer <= 0)
+			{
+				collided = false;
+				TakeAction(dir);
+			}
+		}
 	}
 
 	public void EnableActions(int direction){
@@ -47,15 +62,42 @@ public class DecisionBehaviour : MonoBehaviour {
 		}
 	}
 
+	public void TakeAction(int direction) {
+		switch(direction){
+		case 0:
+			NActionSet[0].GetComponent<Action>().startAction();
+			break;
+		case 1:
+			EActionSet[0].GetComponent<Action>().startAction();
+			break;
+		case 2:
+			SActionSet[0].GetComponent<Action>().startAction();
+			break;
+		case 3:
+			WActionSet[0].GetComponent<Action>().startAction();
+			break;
+		}
+		DisableActions ();
+	}
+
 	public void DisableActions(){
 		foreach(GameObject a in GameObject.FindGameObjectsWithTag("Action"))
 			Destroy (a);
 	}
 
+	void OnTriggerExit(Collider col) {
+		collided = false;
+		text.text = "";
+
+	}
+
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.tag == "MainCamera"){
 			print("Collision!");
-			EnableActions(col.gameObject.GetComponent<MoveCamera>().direction);
+			dir = col.gameObject.GetComponent<MoveCamera>().direction;
+			EnableActions(dir);
+			collided = true;
+			timer = pickTime;
 		}
 	}
 }
