c4269902ff7902b193f257d08bfffca563bb9153
Will Sawyer <wasawyer@umich.edu>
2014-11-09 18:43:40 -0500

    New ground / wall assets from standard library

---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	public string title = "Powerup";
+	public string message = "You got powerupped";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void action (MoveCamera playerToAffect){
+
+	}
+}
