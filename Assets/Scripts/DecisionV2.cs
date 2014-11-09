d2bd8381ee32bf802d70520b2649830e1d02332f
Jacob <texel@umich.edu>
2014-11-09 12:52:50 -0500

    Player script now handles player input and tracking of available actions. This was previously handled by the decision script. Added string fields to player script to allow you to set the input keys for a player. Alterred how respawning works.

--- Assets/Scripts/DecisionV2.cs
+++ Assets/Scripts/DecisionV2.cs
@@ -12,114 +12,19 @@ public class DecisionV2 : MonoBehaviour {
 	public Options EActionSet;
 	public Options SActionSet;
 	public Options WActionSet;
-	private Options current;
-
-
-	// Stuff for the countdown
-	public float pickTime = 3f;
-	public GUIText text;
-	private float timer;
-	private int dir;
-
-	// Information about collider
-	private bool collided;
-	private int playerNum;
-	private Collider playerCol;
-
 
 	// Use this for initialization
 	void Start () {
-		collided = false;
 		gameObject.renderer.material.color = new Color(1.0f,1.0f,1.0f,0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(collided) {
-			if(current == null){
-				Debug.LogError("Player options currently null");
-				return;
-			}
-			if(current.backward && (Input.GetButtonDown ("backward") && playerNum == 1 || Input.GetButtonDown ("backward2") && playerNum == 2)) {
-				playerCol.GetComponent<MoveCamera>().turnAround();
-				actionTaken ();
-			} else if(current.left && (Input.GetButtonDown("left") && playerNum == 1 || Input.GetButtonDown("left2") && playerNum == 2)) {
-				playerCol.GetComponent<MoveCamera>().turnLeft ();
-				actionTaken ();
-			} else if(current.right && (Input.GetButtonDown("right") && playerNum == 1 || Input.GetButtonDown("right2") && playerNum == 2)) {
-				playerCol.GetComponent<MoveCamera>().turnRight ();
-				actionTaken ();
-			} else if(current.forward && (Input.GetButtonDown("forward") && playerNum == 1 || Input.GetButtonDown("forward2") && playerNum == 2)) {
-				playerCol.GetComponent<MoveCamera>().continueMoving();
-				actionTaken ();
-			} else {
-				timer -= Time.deltaTime;
-				text.text = timer.ToString ();
-				if (timer <= 0)
-				{
-					collided = false;
-					TakeAction(dir);
-				}
-			}
-		}
-	}
 
-	public void TakeAction(int direction) {
-		//Forward, right, left, backward
-		if (current.forward) {
-			playerCol.gameObject.GetComponent<MoveCamera>().continueMoving();
-		} else if(current.left) {
-			playerCol.gameObject.GetComponent<MoveCamera>().turnLeft();
-		} else if(current.right){
-			playerCol.GetComponent<MoveCamera>().turnRight();
-		} else if(current.backward){
-			playerCol.GetComponent<MoveCamera>().turnAround();
-		} else {
-			Debug.Log("No movement option was available to player "+playerCol.gameObject.name);
-		}
-		actionTaken ();
-	}
-
-	void OnTriggerExit(Collider col) {
-		actionTaken ();
 	}
 
 	void OnTriggerEnter(Collider col){
-		if(col.gameObject.tag == "Player"){
-			if(col.gameObject.name == "Player1Cam")
-				playerNum = 1;
-			else {
-				playerNum = 2;
-			}
-			print("Collision!");
-			playerCol = col;
-			dir = col.gameObject.GetComponent<MoveCamera>().direction;
-			text = col.gameObject.GetComponent<MoveCamera>().messageText;
-			switch(dir){
-			case 0:
-				current = NActionSet;
-				break;
-			case 1:
-				current = EActionSet;
-				break;
-			case 2:
-				current = SActionSet;
-				break;
-			case 3:
-				current = WActionSet;
-				break;
-			default:
-				Debug.Log ("Unrecognized direction for player collision");
-				break;
-			}
-			collided = true;
-			timer = pickTime;
-		}
-	}
 
-	void actionTaken(){
-		collided = false;
-		text.text = "";
 	}
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,20 @@ public class Player : MonoBehaviour {
 	public int score;
 	public bool cat; // Sets the player as the "Cat" so he can eat the mice
 	public bool respawning;
+	public bool collided;
 	public GUIText scoreText;
+	public GUIText timerText;
+	public Options current;
+	private float timer;
+	public float pickTime = 3f;
+
+
+	//Input strings for movement
+	public string forward;
+	public string left;
+	public string right;
+	public string backward;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,13 +32,42 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {		
 		scoreText.text = score.ToString ();
+		if(collided) {
+			if(current == null){
+				Debug.LogError("Player options currently null");
+				return;
+			}
+			if(current.backward && Input.GetButtonDown (backward)) {
+				GetComponent<MoveCamera>().turnAround();
+				actionTaken ();
+			} else if(current.left && Input.GetButtonDown(left)) {
+				GetComponent<MoveCamera>().turnLeft ();
+				actionTaken ();
+			} else if(current.right && Input.GetButtonDown(right)) {
+				GetComponent<MoveCamera>().turnRight ();
+				actionTaken ();
+			} else if(current.forward && Input.GetButtonDown(forward)) {
+				GetComponent<MoveCamera>().continueMoving();
+				actionTaken ();
+			} else {
+				timer -= Time.deltaTime;
+				timerText.text = timer.ToString ();
+				if (timer <= 0)
+				{
+					collided = false;
+					TakeAction();
+				}
+			}
+		}
 	}
 
 	public void respawn() {
 		gameObject.renderer.enabled = true;
-		respawning = false;
 		GameObject[] points = GameObject.FindGameObjectsWithTag ("Decision");
 		transform.position = points [Random.Range (0, points.Length - 1)].transform.position;
+		GetComponent<Camera> ().enabled = true;
+		respawning = false;
+
 	}
 
 	void OnTriggerEnter(Collider col) {
@@ -42,14 +84,61 @@ public class Player : MonoBehaviour {
 			}
 			cat = true;
 			col.gameObject.GetComponent<PowerupAction>().startRespawn();
+		} else if (col.tag == "Decision") {
+			print("Collision!");
+			timerText = GetComponent<MoveCamera>().messageText;
+			switch(GetComponent<MoveCamera>().direction){
+			case 0:
+				current = col.gameObject.GetComponent<DecisionV2>().NActionSet;
+				break;
+			case 1:
+				current = col.gameObject.GetComponent<DecisionV2>().EActionSet;
+				break;
+			case 2:
+				current = col.gameObject.GetComponent<DecisionV2>().SActionSet;
+				break;
+			case 3:
+				current = col.gameObject.GetComponent<DecisionV2>().WActionSet;
+				break;
+			default:
+				Debug.Log ("Unrecognized direction for player collision");
+				break;
+			}
+			collided = true;
+			timer = pickTime;
 		}
 	}
 
 	public void startRespawn() {
+		GetComponent<MoveCamera> ().stopMoving ();
+		GetComponent<Camera> ().enabled = false;
+		actionTaken ();
 		if(!respawning) {
 			respawning = true;
 			gameObject.renderer.enabled = false;
 			Invoke("respawn", 2f);
 		}
 	}
+
+	public void TakeAction() {
+		//Forward, right, left, backward
+		if (current.forward) {
+			GetComponent<MoveCamera>().continueMoving();
+		} else if(current.left) {
+			GetComponent<MoveCamera>().turnLeft();
+		} else if(current.right){
+			GetComponent<MoveCamera>().turnRight();
+		} else if(current.backward){
+			GetComponent<MoveCamera>().turnAround();
+		} else {
+			Debug.Log("No movement option was available to player "+name);
+		}
+		actionTaken ();
+	}
+	
+	void actionTaken(){
+		collided = false;
+		timerText.text = "";
+	}
+
 }
