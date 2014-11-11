918ade410d864eae1fe14c6b51991164f33f401d
Jacob <texel@umich.edu>
2014-11-11 16:32:09 -0500

    Added fireball for powered up guy

---
+++ Assets/Scripts/V2/Fireball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	public Vector3 direction;
+	public Color color;
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		//Ensure direction vector is a unit vector
+		direction = direction / direction.magnitude;
+		GetComponent<ParticleSystem> ().startColor = color;
+		renderer.material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "MainCamera") {
+			col.gameObject.GetComponent<MovementV2>().GetKnockedUp(transform.position);
+		}
+		print ("called");
+		Destroy (this.gameObject);
+	}
+}
--- Assets/Scripts/V2/MovementV2.cs
+++ Assets/Scripts/V2/MovementV2.cs
@@ -13,6 +13,8 @@ public class MovementV2 : MonoBehaviour {
 	public float timer;
 	public int points = 0;
 
+	public GameObject fireballPrefab;
+
 	//Bools concerning state of player
 	private bool dash = false;
 	private bool recharge;
@@ -30,11 +32,27 @@ public class MovementV2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(transform.position.y <= 0){
+			startRespawn();
+			return;
+		}
+		//Dash action
 		if (!dash && !pointMan && Input.GetButton (left) && Input.GetButton (right) && !recharge) {
 			dash = true;
 			timer = 0.0f;
 			gameObject.layer = 0;
-		}else if(!knockedUp){
+		} 
+		//Fireball action
+		else if(pointMan && !recharge && Input.GetButton (left) && Input.GetButton (right)){
+			GameObject fireball = Instantiate(fireballPrefab) as GameObject;
+			fireball.transform.position = transform.position+transform.forward;
+			fireball.GetComponent<Fireball>().direction = transform.forward;
+			fireball.GetComponent<Fireball>().color = renderer.material.color;
+			recharge = true;
+			Invoke("rechargeSkill", 0.5f);
+		}
+		//Regular action
+		else if(!knockedUp){
 			if(Input.GetButton(left)){
 				transform.Rotate(100 * Vector3.down * Time.deltaTime);
 			}
@@ -48,29 +66,29 @@ public class MovementV2 : MonoBehaviour {
 				transform.position -= transform.forward * 3 * Time.deltaTime;
 			}
 		}
-		if(dash && !recharge && !knockedUp){
+		if(dash && !recharge){
 			transform.position += transform.forward * 5 * Time.deltaTime;
 			timer += Time.deltaTime;
 			if(timer >= dashTime){
 				dash = false;
 				gameObject.layer = 12; //Layer not rendered by any player or minimap
 				recharge = true;
-				Invoke("rechargeDash", 1.5f);
+				Invoke("rechargeSkill", 1.5f);
 			}
 		}
 		if(knockedUp){
 			if(up){
-				transform.position += 3*knockUpDirection*Time.deltaTime;
-				transform.position += 2*Vector3.up*Time.deltaTime;
+				transform.position += 5 *knockUpDirection*Time.deltaTime;
+				transform.position += 4	*Vector3.up*Time.deltaTime;
 				if(transform.position.y >= 2.0f)
 					up=false;
  			}else{
 				transform.position += 3*knockUpDirection*Time.deltaTime;
-				transform.position += 2*Vector3.down*Time.deltaTime;
-				if(Physics.Raycast(transform.position, Vector3.down, 0.6f)){
+				transform.position += 4*Vector3.down*Time.deltaTime;
+				if(Physics.Raycast(transform.position, Vector3.down, 0.3f)){
 					knockedUp = false;
 					gameObject.layer = 12;
-				} else if(transform.position.y <= -1){
+				} else if(transform.position.y <= 0){
 					knockedUp = false;
 					gameObject.layer = 12;
 					startRespawn();
@@ -79,7 +97,7 @@ public class MovementV2 : MonoBehaviour {
 		}
 	}
 
-	void rechargeDash() {
+	void rechargeSkill() {
 		recharge = false;
 	}
 
@@ -132,7 +150,7 @@ public class MovementV2 : MonoBehaviour {
 	}
 
 	void startRespawn() {
-		GetComponent<Camera> ().enabled = false;
+		GetComponent<Camera>().enabled = false;
 		if(!respawning) {
 			respawning = true;
 			gameObject.renderer.enabled = false;
