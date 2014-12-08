154c2820ec0b915c8c524264aa16ed4432dd79da
Will Sawyer <wasawyer@umich.edu>
2014-12-07 21:53:19 -0500

    New map - Scene is _New_Will

---
+++ Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMovement : MonoBehaviour {
+	public float speed = 4f;
+	public float upperBound = 10f;
+	public float lowerBound = -10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Basic Movement
+		Vector3 pos = transform.position; 
+		pos.y += speed * Time.deltaTime; 
+		transform.position = pos;
+		
+		//Changing Directions
+		if ( pos.y < lowerBound ) 
+		{ 
+			speed = Mathf.Abs( speed); // Move up 
+		} 
+		else if ( pos.y > upperBound ) 
+		{
+			speed = -Mathf.Abs( speed); // Move down 
+		}
+	}
+}
